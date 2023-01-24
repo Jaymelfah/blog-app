e179c22a5c163c907ee2b13f3bcb2cbe7c22e401
Jeremiah Melfah <jeremiahmelfah@Jeremiahs-MacBook-Air.local>
2023-01-24 06:37:07 +0000

    Add author references as required in changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_23_193341) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_24_062500) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +41,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_23_193341) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -58,4 +60,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_23_193341) do
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
+  add_foreign_key "posts", "users", column: "author_id"
 end
