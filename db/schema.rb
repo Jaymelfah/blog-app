db6b7e2ad7b40a8f7e7a6c86ca7aa4b356ea379a
Jeremiah Melfah <jeremiahmelfah@Jeremiahs-MacBook-Air.local>
2023-01-24 13:38:52 +0000

    add methods in all models and test in rails console

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_24_062500) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_24_115440) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_24_062500) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.bigint "post_id", null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
@@ -57,6 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_24_062500) do
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
+  add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
