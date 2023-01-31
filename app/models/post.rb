91aa51da81d862f5ea3745b0eca82065d1b845e1
Jeremiah Melfah <jeremiahmelfah@Jeremiahs-MacBook-Air.local>
2023-01-31 12:35:34 +0000

    Add views for posts and users

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,4 +15,8 @@ class Post < ApplicationRecord
   def five_recent_comments
     comments.order(created_at: :desc).limit(5)
   end
+
+  def all_comments
+    comments.order(created_at: :desc)
+  end
 end
