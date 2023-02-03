6540786d005b2e904ddd9df10def55290c5d4a87
Jeremiah Melfah <jeremiahmelfah@Jeremiahs-MacBook-Air.local>
2023-02-03 13:28:24 +0000

    clean code

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,7 @@
 class LikesController < ApplicationController
   def create
-    @user = current_user
     @post = Post.find(params[:post_id])
-    @like = Like.new(user: current_user, post_id: params[:post_id])
-    @like.user_id = @user.id
-    @like.post_id = params[:post_id]
-    redirect_to user_post_path(@user, @post) if @like.save
+    @like = Like.new(user_id: current_user.id, post_id: params[:post_id])
+    redirect_to user_post_path(current_user, @post) if @like.save
   end
 end
