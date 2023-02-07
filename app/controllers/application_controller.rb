b118fff819dd0119ff1bfc5ef7801f1bc5a1c6b6
Jeremiah Melfah <jeremiahmelfah@Jeremiahs-MacBook-Air.local>
2023-02-07 08:32:03 +0000

    fix bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  # def after_sign_up_path_for(resource_or_scope)
+  #   if resource_or_scope.is_a?(User)
+  #     users_url
+  #   else
+  #     super
+  #   end
+  # end
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password)}
+  end
 end
