e8228c26ac9ffc53e5b13bb266ba7fada06ab7d9
Jeremiah Melfah <jeremiahmelfah@Jeremiahs-MacBook-Air.local>
2023-02-07 06:45:46 +0000

    Add devise to project

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'users#index'
+  
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
@@ -7,5 +10,5 @@ Rails.application.routes.draw do
     end
   end
   # Defines the root path route ("/")
-   root 'users#index'
+   
 end
