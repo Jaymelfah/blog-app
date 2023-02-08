afe9e621ff1ba5dae44eec9b883735579518e57f
Jeremiah Melfah <jeremiahmelfah@Jeremiahs-MacBook-Air.local>
2023-02-08 15:53:47 +0000

    Fix linters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,11 +1,9 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
     can :read, Post
-    
+
     return unless user.present?
 
     can :manage, Post, author_id: user.id
